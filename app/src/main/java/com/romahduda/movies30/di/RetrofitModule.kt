0b371d7f38e7cd069ad21eb47ad9d57eadf65f60
RocketMan2k21 <romahaduda@gmail.com>
2024-07-23 19:06:49 +0300

    Added hilt retrofit module

---
+++ app/src/main/java/com/romahduda/movies30/di/RetrofitModule.kt
@@ -0,0 +1,32 @@
+package com.romahduda.movies30.di
+
+import com.romahduda.movies30.data.model.MoviesApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    fun provideBaseUrl():String =  "https://api.themoviedb.org/3/"
+
+    @Provides
+    @Singleton
+    private fun provideRetrofit(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): MoviesApi= retrofit.create(MoviesApi::class.java)
+
+}
